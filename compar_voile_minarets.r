c9abfd17e19dafd9f1b649eb051e3f756d56ff20
awp-finanznachrichten <sw@awp.ch>
2021-02-21 17:48:13 +0100

    commit from Rstudio

---
+++ compar_voile_minarets.r
@@ -0,0 +1,88 @@
+voile21_raw <- read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_maerz2021/master/Output/Verhuellungsverbot_dw.csv")
+
+mina09_raw <-  read_excel("Data/daten_minarett_bfs-2.xls")
+
+#Wrangling
+mina09 <- mina09_raw %>%
+  select(3,11)
+
+#en français
+voile21_fr <- voile21_raw %>%
+  select(1,3,4,6) %>%
+  left_join(mina09, by = c("Gemeinde_Nr" = "Gemeinde-Nr.")) %>%
+  mutate(ecart = Ja_Stimmen_In_Prozent - `Ja in %`)
+
+augment_fr <- voile21_fr %>%
+  arrange(desc(ecart)) %>%
+  select(4,2,5,6) %>%
+  rename(Commune = Gemeinde_KT_f,
+         'Oui à l\'interdiction de se dissimuler le visage dans l\'espace public (2021)' = Ja_Stimmen_In_Prozent,
+         'Oui à l\'interdiction de construire des minarets (2009)' = 'Ja in %',
+         'Ecart (en points de pourcentage)' = ecart) %>%
+  slice(1:50)
+
+diminu_fr <- voile21_fr %>%
+  arrange(ecart) %>%
+  select(4,2,5,6) %>%
+  rename(Commune = Gemeinde_KT_f,
+         'Oui à l\'interdiction de se dissimuler le visage dans l\'espace public (2021)' = Ja_Stimmen_In_Prozent,
+         'Oui à l\'interdiction de construire des minarets (2009)' = 'Ja in %',
+         'Ecart (en points de pourcentage)' = ecart) %>%
+  slice(1:50)
+
+write.csv(augment_fr,"Tableaux/augment_fr.csv", fileEncoding = "UTF-8")
+write.csv(diminu_fr, "Tableaux/diminu_fr.csv", fileEncoding = "UTF-8")
+
+#en allemand
+voile21_de <- voile21_raw %>%
+  select(1,3,4,5) %>%
+  left_join(mina09, by = c("Gemeinde_Nr" = "Gemeinde-Nr.")) %>%
+  mutate(ecart = Ja_Stimmen_In_Prozent - `Ja in %`)
+
+augment_de <- voile21_de %>%
+  arrange(desc(ecart)) %>%
+  select(4,2,5,6) %>%
+  rename(Gemeinde = Gemeinde_KT_d,
+         'Oui à l\'interdiction de dissimuler son visage dans l\'espace public (2021)' = Ja_Stimmen_In_Prozent,
+         'Oui à l\'interdiction de construire des minarets (2009)' = 'Ja in %',
+         'Ecart (en points de pourcentage)' = ecart) %>%
+  slice(1:50)
+
+diminu_de <- voile21_de %>%
+  arrange(ecart) %>%
+  select(4,2,5,6) %>%
+  rename(Gemeinde = Gemeinde_KT_d,
+         'Oui à l\'interdiction de dissimuler son visage dans l\'espace public (2021)' = Ja_Stimmen_In_Prozent,
+         'Oui à l\'interdiction de construire des minarets (2009)' = 'Ja in %',
+         'Ecart (en points de pourcentage)' = ecart) %>%
+  slice(1:50)
+
+write.csv(augment_de,"Tableaux/augment_de.csv", fileEncoding = "UTF-8")
+write.csv(diminu_de, "Tableaux/diminu_de.csv", fileEncoding = "UTF-8")
+
+#en italien
+voile21_it <- voile21_raw %>%
+  select(1,3,4,7) %>%
+  left_join(mina09, by = c("Gemeinde_Nr" = "Gemeinde-Nr.")) %>%
+  mutate(ecart = Ja_Stimmen_In_Prozent - `Ja in %`)
+
+augment_it <- voile21_it %>%
+  arrange(desc(ecart)) %>%
+  select(4,2,5,6) %>%
+  rename(Comune = Gemeinde_KT_i,
+         'Sì all\'iniziativa anti-burqa (2021)' = Ja_Stimmen_In_Prozent,
+         'Sì al divieto di costruzione di minareti (2009)' = 'Ja in %', 
+         'Scarto (in punti percentuali)' = ecart) %>% 
+  slice(1:50)
+
+diminu_it <- voile21_it %>%
+  arrange(ecart) %>%
+  select(4,2,5,6) %>%
+  rename(Comune = Gemeinde_KT_i,
+         'Sì all\'iniziativa anti-burqa (2021)' = Ja_Stimmen_In_Prozent,
+         'Sì al divieto di costruzione di minareti (2009)' = 'Ja in %', 
+         'Scarto (in punti percentuali)' = ecart) %>% 
+  slice(1:50)
+
+write.csv(augment_it,"Tableaux/augment_it.csv", fileEncoding = "UTF-8")
+write.csv(diminu_it, "Tableaux/diminu_it.csv", fileEncoding = "UTF-8")
--- top_flop.r
+++ top_flop.r
@@ -21,7 +21,7 @@ voile_top_de <- voile_raw %>%
   slice(1:50) %>%
   select(5,3) %>%
   rename(Gemeinde = Gemeinde_KT_d,
-         "Ja-Anteil" = Ja_Stimmen_In_Prozent)
+         "Ja in %" = Ja_Stimmen_In_Prozent)
 
 write.csv(voile_top_de,"Tableaux/voile_top_de.csv", fileEncoding = "UTF-8")
 
@@ -31,7 +31,7 @@ voile_top_it <- voile_raw %>%
   slice(1:50) %>%
   select(7,3) %>%
   rename(Municipio = Gemeinde_KT_i,
-         "Per cento sì" = Ja_Stimmen_In_Prozent)
+         "Percentuale di sì" = Ja_Stimmen_In_Prozent)
 
 write.csv(voile_top_it,"Tableaux/voile_top_it.csv", fileEncoding = "UTF-8")
 
@@ -53,7 +53,7 @@ voile_flop_de <- voile_raw %>%
   slice(1:50) %>%
   select(5,4) %>%
   rename(Gemeinde = Gemeinde_KT_d,
-         "Nein-Anteil" = Nein_Stimmen_In_Prozent)
+         "Nein in %" = Nein_Stimmen_In_Prozent)
 
 write.csv(voile_flop_de,"Tableaux/voile_flop_de.csv", fileEncoding = "UTF-8")
 
@@ -63,12 +63,9 @@ voile_flop_it <- voile_raw %>%
   slice(1:50) %>%
   select(7,4) %>%
   rename(Municipio = Gemeinde_KT_i,
-         "Per cento no" = Nein_Stimmen_In_Prozent)
+         "Percentuale di no" = Nein_Stimmen_In_Prozent)
 
 write.csv(voile_flop_it,"Tableaux/voile_flop_it.csv", fileEncoding = "UTF-8")
-#Hasle LU LU
-
-
 
 ##E-ID
 eid_raw <-  read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_maerz2021/master/Output/E-ID-Gesetz_dw.csv")
@@ -89,7 +86,7 @@ eid_top_de <- eid_raw %>%
   slice(1:50) %>%
   select(5,3) %>%
   rename(Gemeinde = Gemeinde_KT_d,
-         "Ja-Anteil" = Ja_Stimmen_In_Prozent)
+         "Ja in %" = Ja_Stimmen_In_Prozent)
 
 write.csv(eid_top_de,"Tableaux/eid_top_de.csv", fileEncoding = "UTF-8")
 
@@ -99,7 +96,7 @@ eid_top_it <- eid_raw %>%
   slice(1:50) %>%
   select(7,3) %>%
   rename(Municipio = Gemeinde_KT_i,
-         "Per cento sì" = Ja_Stimmen_In_Prozent)
+         "Percentuale di sì" = Ja_Stimmen_In_Prozent)
 
 write.csv(eid_top_it,"Tableaux/eid_top_it.csv", fileEncoding = "UTF-8")
 
@@ -121,7 +118,7 @@ eid_flop_de <- eid_raw %>%
   slice(1:50) %>%
   select(5,4) %>%
   rename(Gemeinde = Gemeinde_KT_d,
-         "Nein-Anteil" = Nein_Stimmen_In_Prozent)
+         "Nein in %" = Nein_Stimmen_In_Prozent)
 
 write.csv(eid_flop_de,"Tableaux/eid_flop_de.csv", fileEncoding = "UTF-8")
 
@@ -131,7 +128,7 @@ eid_flop_it <- eid_raw %>%
   slice(1:50) %>%
   select(7,4) %>%
   rename(Municipio = Gemeinde_KT_i,
-         "Per cento no" = Nein_Stimmen_In_Prozent)
+         "Percentuale di no" = Nein_Stimmen_In_Prozent)
 
 write.csv(eid_flop_it,"Tableaux/eid_flop_it.csv", fileEncoding = "UTF-8")
 
@@ -157,7 +154,7 @@ indo_top_de <- indo_raw %>%
   slice(1:50) %>%
   select(5,3) %>%
   rename(Gemeinde = Gemeinde_KT_d,
-         "Ja-Anteil" = Ja_Stimmen_In_Prozent)
+         "Ja in %" = Ja_Stimmen_In_Prozent)
 
 write.csv(indo_top_de,"Tableaux/indo_top_de.csv", fileEncoding = "UTF-8")
 
@@ -167,7 +164,7 @@ indo_top_it <- indo_raw %>%
   slice(1:50) %>%
   select(7,3) %>%
   rename(Municipio = Gemeinde_KT_i,
-         "Per cento sì" = Ja_Stimmen_In_Prozent)
+         "Percentuale di sì" = Ja_Stimmen_In_Prozent)
 
 write.csv(indo_top_it,"Tableaux/indo_top_it.csv", fileEncoding = "UTF-8")
 
@@ -189,7 +186,7 @@ indo_flop_de <- indo_raw %>%
   slice(1:50) %>%
   select(5,4) %>%
   rename(Gemeinde = Gemeinde_KT_d,
-         "Nein-Anteil" = Nein_Stimmen_In_Prozent)
+         "Nein in %" = Nein_Stimmen_In_Prozent)
 
 write.csv(indo_flop_de,"Tableaux/indo_flop_de.csv", fileEncoding = "UTF-8")
 
@@ -199,6 +196,6 @@ indo_flop_it <- indo_raw %>%
   slice(1:50) %>%
   select(7,4) %>%
   rename(Municipio = Gemeinde_KT_i,
-         "Per cento no" = Nein_Stimmen_In_Prozent)
+         "Percentuale di no" = Nein_Stimmen_In_Prozent)
 
 write.csv(indo_flop_it,"Tableaux/indo_flop_it.csv", fileEncoding = "UTF-8")
